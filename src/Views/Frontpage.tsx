26b37f9ec43c2547f98dc7380f2d2a4d54b6f188
laurens.mesure <laurens.mesure@student.howest.be>
2021-04-21 10:37:30 +0200

    Added dark theme to url tester

--- src/Views/Frontpage.tsx
+++ src/Views/Frontpage.tsx
@@ -4,9 +4,6 @@ import UrlTester from "../Components/UrlTester";
 const Frontpage: React.FC = () => {
     return (
         <div className="p-4">
-            <h1 className="text-center text-lg mb-4">
-                You can test any URL for malware, phishing, scam...
-            </h1>
             <UrlTester />
         </div>
     );
