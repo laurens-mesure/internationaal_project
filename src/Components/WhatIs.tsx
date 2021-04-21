76bf3223401c7aa8f17154e8c6010fe5135268a3
Cédric <cedric.maes3@student.howest.be>
2021-04-21 09:42:07 +0200

    change why phishing into what is phishing

--- src/Components/WhatIs.tsx
+++ src/Components/WhatIs.tsx
@@ -3,11 +3,23 @@ import React from "react";
 const WhatIs: React.FC = () => {
     return (
         <section className="mx-auto w-11/12 sm:w-2/3 xl:w-1/3 text-gray-700 text-lg mb-4 mt-4">
-            <h1 className="text-2xl 2xl:font-bold mb-4 mt-2">Why phishing?</h1>
-            <p className="mb-3">Phishing is used by criminals for financial gain.</p>
-            <p className="mb-3">Therefore, the attackers steal personal information from the victim - such as <strong>credit card numbers,
-                login credentials, ...</strong> - and sell that information</p>
-            <p className="mb-3">It is also often used to gain a foothold in corporate or governmental networks as a part of a larger attack.</p>
+            <h1 className="text-2xl 2xl:font-bold mb-4 mt-2">What is phishing?</h1>
+            <p className="mb-3">Phishing is a type of online scam where criminals impersonate legitimate organizations
+                via email, text message, advertisement or other means in order to steal sensitive personal data.</p>
+            <p> Some examples of this sensitive, personal data are:</p>
+            <ul className="list-disc ml-20 mb-3">
+                <li>passwords</li>
+                <li>phone numbers</li>
+                <li>email addresses</li>
+                <li>credit card numbers</li>
+                <li>...</li>
+            </ul>
+            <p className="mb-3">Usually, the attacker uses email spoofing or text messaging to reach his victims.
+                Often, the attacker leads the victim to a fake website which matches the look and feel
+                of the legitimate site. As of 2020, phishing is the most common attack performed by cyber criminals.
+            </p>
+            <p className="mb-3">Such an attack can be disastrous to the victim. It can lead to identity theft,
+                unauthorized purchases, ...</p>
         </section>
     );
 };
