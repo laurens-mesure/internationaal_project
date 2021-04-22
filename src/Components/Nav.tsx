217a3b2576ce507885721508ec3ab172febd90a6
siebe.vervoort <siebe.vervoort@student.howest.be>
2021-04-22 15:03:10 +0200

    more responsivenesss

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 // Icons
 import HomeIcon from "../assets/img/home";
@@ -8,7 +8,7 @@ import PhishingHeaderImg from "../assets/img/phishing-banner.jpg";
 
 const Nav: React.FC = () => {
     return (
-        <header className="bg-gray-900 header mb-10" >
+        <header className="bg-gray-900 header mb-10">
             <nav className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
                 <div className="flex items-center justify-between">
                     <a className="text-xl font-bold text-white transition-colors duration-300 transform md:text-2xl hover:text-blue-400"
@@ -16,10 +16,11 @@ const Nav: React.FC = () => {
 
                 </div>
 
-                <div className="flex-col mt-2 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+                <div
+                    className="flex-col mt-2 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
 
                     <ul className="flex flex-row items-center justify-center">
-                        <NavItem route="/" title="Home" icon={HomeIcon} />
+                        <NavItem route="/" title="Home" icon={HomeIcon}/>
                         <NavItem
                             route="/breaches"
                             title="Breaches"
@@ -30,9 +31,11 @@ const Nav: React.FC = () => {
                 </div>
             </nav>
 
-            <section className="flex items-center justify-center" style={{height:650, backgroundImage: `url(${PhishingHeaderImg})`, backgroundPosition:`center`}}>
+            <section className="flex items-center justify-center"
+                     style={{height: 650, backgroundImage: `url(${PhishingHeaderImg})`, backgroundPosition: `center`}}>
                 <div className="text-center">
-                    <h2 className="text-3xl font-bold text-white md:text-6xl bg-gray-900 p-2 rounded-lg mx-3"><b>Phishing...</b> Don't get hooked!</h2>
+                    <h2 className="text-3xl font-bold text-white md:text-6xl bg-gray-900 p-2 rounded-lg mx-3">
+                        <b>Phishing...</b> Don't get hooked!</h2>
                 </div>
             </section>
         </header>
@@ -45,14 +48,14 @@ interface INavItem {
     icon: JSX.Element;
 }
 
-const NavItem: React.FC<INavItem> = ({ title, route, icon }) => {
+const NavItem: React.FC<INavItem> = ({title, route, icon}) => {
     return (
-        <li className="border border-gray-300 rounded-md px-5 py-2 hover:border-blue-500 mx-3 my-3">
-            <Link to={route} className="flex flex-row items-center">
+        <Link to={route} className="border border-gray-300 rounded-md px-5 py-2  hover:border-blue-500 mx-3 my-3">
+            <li className="flex flex-row items-center">
                 <div className="h-4 w-4">{icon}</div>
                 <p className="px-2 text-white">{title}</p>
-            </Link>
-        </li>
+            </li>
+        </Link>
     );
 };
 
